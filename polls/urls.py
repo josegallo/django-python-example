5b26e99d9a654ef6c573de42d76b7b316552a395
josegallo <josegallo@gmail.com>
2016-10-07 10:29:53 +0200

    Calling the url detail in a better way and namespace
    - href = {% url "polls:detail" question.id %} instead of href="/polls/{{question.id}}
    - Add namespace in mysite/urls.py and polls/templates/polls/index.html

--- polls/urls.py
+++ polls/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
     #the url will be 127.0.0.1/polls/any_number, ex: 127.0.0.1/polls/1
     url(r'^(?P<question_id>[0-9]+)/results$', views.results, name="result"),
-    #the url will be 127.0.0.1/any number/results/
+    #the url will be 127.0.0.1/polls/any number/results/
     url(r'^(?P<question_id>[0-9]+)/vote$', views.vote, name="vote"),
-    # 127.0.0.1/1/vote/
+    # 127.0.0.1/polls/1/vote/
 ]
