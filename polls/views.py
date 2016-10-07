45af00d3e241b8e010c30a2bcdae1c425a314c11
josegallo <josegallo@gmail.com>
2016-10-07 18:16:40 +0200

    Add result of votes and vote again - Modify the functions results and vote - Refactoring of detail function

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from .models import Question, Choice
 # Create your views here.
 
 def index(request):
@@ -16,9 +17,24 @@ def detail(request, question_id):
     context = {'question': question}
     return render(request, 'polls/detail.html', context)
 
+
 def results(request, question_id):
-    return HttpResponse('This are the results of the question: %s' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
 
 def vote(request, question_id):
-    return HttpResponse("Vote on question: %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        # request.POST[choice] retrieves the value of choice, that's the id. (see
+        # form in the detail.html template
+    except:
+        context = {'question': question, 'error_message': 'Please select a choice'}
+        return render(request, 'polls/detail.html', context)
+    # if try works
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
 
+        return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
